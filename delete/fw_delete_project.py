b2875c4aadd4d9cd0032fc8ace96ded5b6bee394
Ariana <arianafamiliar@hotmail.com>
2023-09-20 15:25:24 -0400

    Update fw_delete_project.py

--- delete/fw_delete_project.py
+++ delete/fw_delete_project.py
@@ -7,5 +7,6 @@ for proj in fw.projects.iter(limit=10):
     if proj.group == 'test':
         try:
             fw.delete_project(proj.id)
+            print(f'DELETED: {proj.label}')
         except:
             continue
