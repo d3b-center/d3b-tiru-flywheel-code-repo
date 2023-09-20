055e8d97ba531aa9875ca27896b7da7bd7920704
Ariana <arianafamiliar@hotmail.com>
2023-09-20 15:14:28 -0400

    Create fw_delete_project.py

---
+++ delete/fw_delete_project.py
@@ -0,0 +1,11 @@
+
+import flywheel
+
+fw = flywheel.Client()
+
+for proj in fw.projects.iter(limit=10):
+    if proj.group == 'test':
+        try:
+            fw.delete_project(proj.id)
+        except:
+            continue
